e57cddd77b6c534997d0ab17c82ecf8499d0bf4b
Kristóf Steindl <ksteindl@chemaxon.com>
2021-10-22 13:53:30 +0200

    finished 1/2

---
+++ src/main/java/com/ksteindl/adventofcode/codingchallenge2021/task1/Coordinate.java
@@ -0,0 +1,35 @@
+package com.ksteindl.adventofcode.codingchallenge2021.task1;
+
+import java.util.Objects;
+
+public class Coordinate {
+
+    final Integer x;
+    final Integer y;
+
+    public Coordinate(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return Objects.equals(x, that.x) && Objects.equals(y, that.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
--- src/main/java/com/ksteindl/adventofcode/codingchallenge2021/task1/Day1.java
+++ src/main/java/com/ksteindl/adventofcode/codingchallenge2021/task1/Day1.java
@@ -4,7 +4,9 @@ import com.ksteindl.adventofcode.codingchallenge2021.CoCha2021;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Day1 extends CoCha2021 {
 
@@ -24,11 +26,77 @@ public class Day1 extends CoCha2021 {
 
     @Override
     public Object getFirstSolution() {
+//        List<String> lines = fileManager.parseLines(fileName);
+//        String[] elements = lines.get(0).split(", ");
+//        int x = 0;
+//        int y = 0;
+//        int cardinal = 0;
+//        for (int i = 0; i < elements.length; i++) {
+//            logger.info("x: " + x);
+//            logger.info("y: " + y);
+//            logger.info("cardinal: " + cardinal);
+//            logger.info("i: " + i);
+//            logger.info("element: " + elements[i]);
+//            char direction = elements[i].charAt(0);
+//            if (cardinal == 0) {
+//                if (direction =='R') {
+//                    cardinal = 1;
+//                } else {
+//                    cardinal = 3;
+//                }
+//
+//            } else if (cardinal == 1) {
+//                if (direction =='R') {
+//                    cardinal = 2;
+//                } else {
+//                    cardinal = 0;
+//                }
+//            } else if(cardinal == 2) {
+//                if (direction =='R') {
+//                    cardinal = 3;
+//                } else {
+//                    cardinal = 1;
+//                }
+//            } else {
+//                if (direction =='R') {
+//                    cardinal = 0;
+//                } else {
+//                    cardinal = 2;
+//                }
+//            }
+//            Integer amount = Integer.parseInt(elements[i].substring(1));
+//            switch (cardinal) {
+//                case 0:
+//                    y += amount;
+//                    break;
+//                case 1:
+//                    x += amount;
+//                    break;
+//                case 2:
+//                    y -= amount;
+//                    break;
+//                case 3:
+//                    x -= amount;
+//                    break;
+//            }
+//            logger.info("amount: " + amount);
+//        }
+//        logger.info("x: " + x);
+//        logger.info("y: " + y);
+//        logger.info("cardinal: " + cardinal);
+        //return x + y;
+        return -1;
+        //return Math.abs(x) + Math.abs(y);
+    }
+
+    @Override
+    public Object getSecondSolution() {
         List<String> lines = fileManager.parseLines(fileName);
         String[] elements = lines.get(0).split(", ");
         int x = 0;
         int y = 0;
         int cardinal = 0;
+        Set<Coordinate> coordinates = new HashSet<>();
         for (int i = 0; i < elements.length; i++) {
             logger.info("x: " + x);
             logger.info("y: " + y);
@@ -65,16 +133,38 @@ public class Day1 extends CoCha2021 {
             Integer amount = Integer.parseInt(elements[i].substring(1));
             switch (cardinal) {
                 case 0:
-                    y += amount;
+                    for (int j = 1; j < amount + 1; j++) {
+                        y += 1;
+                        if (!coordinates.add(new Coordinate(x, y))) {
+                            logger.info("!!! " + new Coordinate(x, y));
+                        }
+                    }
+
                     break;
                 case 1:
-                    x += amount;
+                    for (int j = 1; j < amount + 1; j++) {
+                        x += 1;
+                        if (!coordinates.add(new Coordinate(x, y))) {
+                            logger.info("!!! " + new Coordinate(x, y));
+                        }
+                    }
+
                     break;
                 case 2:
-                    y -= amount;
+                    for (int j = 1; j < amount + 1; j++) {
+                        y -= 1;
+                        if (!coordinates.add(new Coordinate(x, y))) {
+                            logger.info("!!! " + new Coordinate(x, y));
+                        }
+                    }
                     break;
                 case 3:
-                    x -= amount;
+                    for (int j = 1; j < amount + 1; j++) {
+                        x -= 1;
+                        if (!coordinates.add(new Coordinate(x, y))) {
+                            logger.info("!!! " + new Coordinate(x, y));
+                        }
+                    }
                     break;
             }
             logger.info("amount: " + amount);
@@ -84,11 +174,6 @@ public class Day1 extends CoCha2021 {
         logger.info("cardinal: " + cardinal);
         //return x + y;
         return Math.abs(x) + Math.abs(y);
-    }
-
-    @Override
-    public Object getSecondSolution() {
-        return -1;
     }
 
     @Override
